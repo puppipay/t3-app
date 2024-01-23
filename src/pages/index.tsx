c2ab41af5d3c3e321ff624c3d339d07b5f5c776a
rbelavad <oldintrasettle@gmail.com>
2024-01-23 17:19:44 +0530

    debug type error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { useSession } from "next-auth/react";
 import { useState } from "react";
 import { InfiniteTweetList } from "~/components/InfiniteTweetList";
 //import { NewTweetForm } from "~/components/NewTweetForm";
-import { NewTweetForm } from "~/components/NewTweetForm";
+// import { NewTweetForm } from "~/components/NewTweetForm";
 import { api } from "~/utils/api";
 
 const TABS = ["Recent", "Following"] as const;
@@ -36,7 +36,7 @@ const Home: NextPage = () => {
           </div>
         )}
       </header>
-      <NewTweetForm />
+      {/* <NewTweetForm /> */}
       {selectedTab === "Recent" ? <RecentTweets /> : <FollowingTweets />}
     </>
   );
